4372e95357238bbb081fdcbaf6635051d3645bd6
117marta <117marta@gmail.com>
2022-01-13 21:08:35 +0100

    Person and Movie model

--- project/cinema_app/models.py
+++ project/cinema_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + self.last_name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    director = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='movies_directed')
+    year = models.SmallIntegerField()
+    actor = models.ManyToManyField(Person, related_name='movies_cast')
