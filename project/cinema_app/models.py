65774201e487407ba08588d271fc60c4316ecc0d
117marta <117marta@gmail.com>
2022-03-14 22:21:00 +0100

    Movie model change

--- project/cinema_app/models.py
+++ project/cinema_app/models.py
@@ -2,15 +2,20 @@ from django.db import models
 
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.name
 
-    @property
-    def full_name(self):
-        return self.first_name + ' ' + self.last_name
+    # first_name = models.CharField(max_length=50)
+    # last_name = models.CharField(max_length=50)
+    #
+    # def __str__(self):
+    #     return self.first_name + ' ' + self.last_name
+    #
+    # @property
+    # def full_name(self):
+    #     return self.first_name + ' ' + self.last_name
 
 
 class Movie(models.Model):
